0e183f6ba87b5e18377d81e69559255a8d182c5d
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-23 12:30:47 -0400

    Adds Movies page content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./Nav/Nav";
 import Home from "./Pages/Home/Home";
 import Locations from "./Pages/Locations/Locations";
+import Movies from "./Pages/Movies/Movies";
 import People from "./Pages/People/People";
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Locations />} />
+        <Route path="/movies" element={<Movies />} />
         <Route path="/people" element={<People />} />
         <Route path="/locations" element={<Locations />} />
       </Routes>
--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -1,3 +1,51 @@
+import { useEffect, useState } from "react";
+
 export default function Movies() {
-  return;
+  const [movies, setMovies] = useState([]);
+  const [selectedOption, setSelectedOption] = useState(null);
+  useEffect(() => {
+    fetch("https://resource-ghibli-api-pursuit.onrender.com/films")
+      .then((response) => response.json())
+      .then((result) => {
+        setMovies(result);
+        console.log(result);
+      });
+  }, []);
+
+  const handleMovieChange = (e) => setSelectedOption(e.target.value);
+  const selectedMovieDetails =
+    selectedOption === "" ? "" : movies.find((m) => m.title === selectedOption);
+  console.log(selectedMovieDetails);
+
+  return (
+    <main>
+      <section className="movies">
+        <h2>
+          <strong>Title:</strong> Select a Movie
+        </h2>
+        <select onChange={handleMovieChange}>
+          <option value=""></option>
+          {movies.map((movie) => {
+            const { title, id } = movie;
+            return (
+              <option value={title} key={id}>
+                {title}
+              </option>
+            );
+          })}
+        </select>
+        {selectedOption && (
+          <aside>
+            <h2>Title: {selectedMovieDetails.title}</h2>
+            <h6>
+              <span>Release Date:</span> {selectedMovieDetails.release_date}
+            </h6>
+            <p>
+              <span>Description</span>: {selectedMovieDetails.description}
+            </p>
+          </aside>
+        )}
+      </section>
+    </main>
+  );
 }
