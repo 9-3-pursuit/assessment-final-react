8fddda828790659b5bec52d5d899f351b6e98a83
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-03-23 10:53:17 -0400

    movies with useEffect

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -2,6 +2,13 @@ import { getAllMovies } from "../api/fetch";
 import { useState, useEffect } from "react";
 
 function Movies() {
-    
+  const [allMovies, setAllMovies] = useState([]);
+
+  useEffect(() => {
+    getAllMovies().then((data) => {
+      setAllMovies(data);
+      console.log(data);
+    });
+  }, []);
 }
 export default Movies;
