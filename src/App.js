aa80f60cf47682349810e35bcd0af3e5baf9c1c0
Tafari Excell <excell.tafari@gmail.com>
2023-03-23 10:53:44 -0400

    Pages created and linked

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+// import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import Nav from './Components/Nav.js';
+import Home from './Components/Home.js';
+import Movies from './Components/Movies.js';
+import People from './Components/People.js';
+import Locations from './Components/Locations.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 function App() {
+
+
   return (
-    <div className="app">
-      <h1>Welcome to GhibliApp</h1>
+    <div className="App">
+     <header className="App-header">
+      <Router>
+      <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/locations" element={<Locations />} />
+        </Routes>
+      </Router>
+      </header>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Components/Nav.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+
+
+export default function Nav() {
+  return (
+    <div>
+      <nav>
+        <p>This is the Nav page</p>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/characters">Characters</Link>
+          </li>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
