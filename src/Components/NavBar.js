34bcd9e8e23dbad03095d27acff0a7c792e9de8e
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-03-23 10:44:03 -0400

    pass 11 tests

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import logo from "./Images/Studio_Ghibli_logo.png";
+
 const NavBar = () => {
   return (
     <nav>
       <Link to="/">
-        <img src="" alt="Ghibli Logo" />
+        <img src={logo} alt="Ghibli Logo" />
       </Link>
       <Link to="/movies">Movies</Link>
       <Link to="/people">People</Link>
@@ -11,7 +13,4 @@ const NavBar = () => {
     </nav>
   );
 };
-
 export default NavBar;
-
-
