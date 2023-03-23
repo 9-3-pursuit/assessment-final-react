2ed48c244f4078ac490548dfc9c8b59c339fe20a
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-03-23 10:57:19 -0400

    api is films not movies

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/" element={<Movies />} />
-          <Route path="/" element={<People />} />
-          <Route path="/" element={<Locations />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/locations" element={<Locations />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://resource-ghibli-api-pursuit.onrender.com";
 
 export function getAllMovies() {
-  return fetch(`${BASE_URL}/movies`).then((response) => response.json());
+  return fetch(`${BASE_URL}/films`).then((response) => response.json());
 }
