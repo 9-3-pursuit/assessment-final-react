277d95ad2c07398c5c907ab4f5a36b74db25b2f2
Ryan Lundy <rlundy1792@gmail.com>
2023-03-23 17:37:28 -0400

    Completed People component functionality

--- src/Components/Locations/Locations.jsx
+++ src/Components/Locations/Locations.jsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from "react";
+
+const BASE_URL = "https://resource-ghibli-api-pursuit.onrender.com/locations"
+
+
+
 export default function Locations() {
+
     return (
-        <div>
-            I am the Locations page.
+        <div className="locations" >
+            <h3>List of Locations</h3>
+
         </div>
     )
 }
