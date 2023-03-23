ae91975812aef262a2004d2e7248c0143b39c074
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-03-23 10:30:22 -0400

    import components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import NavBar from "./Components/NavBar"
+import Movies from "./Components/Movies"
+import People from "./Components/People"
+import Locations from "./Components/Locations"
+
 function App() {
   return (
     <div className="app">
