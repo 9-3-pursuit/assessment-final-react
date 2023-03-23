a7632ecdffe7792310ba0e8ec8070e86a8fb2188
Marangely Rosas <Ladykaliber22@gmail.com>
2023-03-23 12:11:41 -0400

    Created navigation bar, included links for "Movies", "People", and "Locations", made sure each link is set to go to its related route

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+
+export default function NavBar() {
+    return (
+        <nav>
+            <article>
+                <Link to="/"><img src={logo} alt="logo"></img></Link>
+            </article>
+            <aside>
+                <ul>
+                    <li>
+                        <Link to="/movies">Movies</Link>
+                    </li>
+
+                    <li>
+                        <Link to="/people">People</Link>
+                    </li>
+
+                    <li>
+                        <Link to="/locations">Locations</Link>
+                    </li>
+                </ul>
+            </aside>
+        </nav>
+    )
+}
