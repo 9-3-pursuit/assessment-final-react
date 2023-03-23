545088dfeff236a49a543d837390187df092f8f2
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-23 12:30:24 -0400

    Adds content for navbar

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
 export default function Nav() {
-  return;
+  return (
+    <nav>
+      <Link to="/"> Logo </Link>
+      <Link to="/movies"> Movies </Link>
+      <Link to="/people"> People </Link>
+      <Link to="/locations"> Locations </Link>
+    </nav>
+  );
 }
