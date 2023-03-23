247ec13192fc6c327f87fc80dc7519b48d04301c
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-03-23 10:30:51 -0400

    fix route error

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
-import NavBar from "./Components/NavBar"
-import Movies from "./Components/Movies"
-import People from "./Components/People"
-import Locations from "./Components/Locations"
+import NavBar from "./Components/NavBar";
+import Movies from "./Components/Movies";
+import People from "./Components/People";
+import Locations from "./Components/Locations";
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
-       
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Movies />} />
+          <Route path="/" element={<People />} />
+          <Route path="/" element={<Locations />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
