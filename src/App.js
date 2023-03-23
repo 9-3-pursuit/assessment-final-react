0dcbc72e2ec69dc977a99309b2deb8c6c29eb809
Marangely Rosas <Ladykaliber22@gmail.com>
2023-03-23 11:34:30 -0400

    Created Components Folder, createdall necessary Component Files inside the Components Folder, imported each Component in App.js file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import NavBar from "./Components/NavBar";
+import Movies from "./Components/Movies";
+import People from "./Components/People";
+import Locations from "./Components/Locations";
+
+
+
 function App() {
   return (
     <div className="app">
