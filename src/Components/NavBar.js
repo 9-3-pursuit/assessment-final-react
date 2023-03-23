4b00d9596092c489e7023c8b7201e2081a1128a6
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-03-23 10:35:31 -0400

    add logo

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
 const NavBar = () => {
-  return <nav></nav>;
+  return (
+    <nav>
+      <Link to="/">
+        <img src="" alt="Ghibli Logo" />
+      </Link>
+    </nav>
+  );
 };
 
 export default NavBar;
