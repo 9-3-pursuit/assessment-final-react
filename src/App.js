39e70d98321f184ed86763640df386aa10c42bb6
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-03-23 14:48:16 -0400

    Built component tree, functional nav and home.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+
 function App() {
   return (
     <div className="app">
-      <h1>Welcome to GhibliApp</h1>
+      <Router>
+      <div className="container">
+        <Nav/>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </div>
+      
+      </Router>
+        
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import img from "./assets/gilbiLogo.jpeg";
+import "./Nav.css"
+
+export default function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <Link className="nav-link fs-3" to="/">
+              <section className="logo d-flex align-items-center">
+                <img className="img-thumbnail logo-img" src={img} alt="Ghilbi logo" />
+              </section>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link fs-3" to="/films">
+              Films
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link fs-3" to="/people">
+              People
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link fs-3" to="/locations">
+              Locations
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
