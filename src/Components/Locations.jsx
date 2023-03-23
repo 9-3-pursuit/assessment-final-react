2b1118e15bea67df5468d4aa8211b948ef37005e
Diandre Miller <millerdiandre@gmail.com>
2023-03-23 17:02:44 -0400

    successfully fetched data for all components

--- src/Components/Locations.jsx
+++ src/Components/Locations.jsx
@@ -1,5 +1,17 @@
-const Locations = () => {
-    return null
-}
+import { useState, useEffect } from "react"
+import { getLocations } from "../API/Fetch"
 
+const Locations = () => {
+
+    const [locations, setLocations] = useState([])
+
+    useEffect(() => {
+        getLocations()
+            .then((data) => {
+                setLocations(data)
+                console.log('locations',data)
+        })
+    },[])
+
+}
 export default Locations
--- src/Components/PeoplePage.jsx
+++ src/Components/PeoplePage.jsx
@@ -1,4 +1,18 @@
+import { useState, useEffect } from 'react'
+import {getPeople} from '../API/Fetch'
+
 const PeoplePage = () => {
+
+    const [people, setPeople] = useState([])
+
+    useEffect(() => {
+        getPeople()
+            .then((data) => {
+                setPeople(data)
+                console.log('people',data)
+        })
+    })
+
     return null
 }
 
