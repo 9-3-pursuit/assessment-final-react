6c80e2811146807fc67ab2c11c3c4be35de06c54
Markeya Mckoy-Carree <mcmcarree@gmail.com>
2023-03-23 11:49:12 -0400

    Some test passed & some css done

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,9 @@ export default function Home(){
         <div>
 
             <h1>Welcome to GhibliApp</h1>
-
+            <img className="gif" src="https://media3.giphy.com/media/F47qASXDMJcOY/200w.gif?cid=82a1493bq7wk6pp6waf8xmnj0uo4ar9j9neltputecbhuuu2&rid=200w.gif&ct=g"></img>
+            <img className="gif2" src="https://media.tenor.com/OxIKfikF9IsAAAAM/s%C3%B3observo-watchin-movie.gif"></img>
+            
         </div>
 
     )
---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export default function Nav(){
+    return (
+        <div className="nav">
+            <nav>
+                <Link to="/">
+                <img src="https://media.gettyimages.com/id/1244034031/vector/cinema-poster-with-cola-film-strip-and-clapper-vector.jpg?s=612x612&w=gi&k=20&c=8ClshQC50T-wPj6CPvnPnFq1Er6Fs8fbrreXWehvdgk=" alt="logo"/>
+                </Link>    
+
+                <Link to="/">Home</Link>
+                <Link to="/Movies">Movies</Link>
+                <Link to="/People">People</Link>
+                <Link to="/Locations">Locations</Link>
+            </nav>
+        </div>
+    )
+}
