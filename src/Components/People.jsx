3d8cd6c91c9e361c2925ef21a8fa82fd60e9f3c3
Fernando Cortez <cortez.fernando11216@gmail.com>
2023-03-24 09:45:38 -0400

    created each component with working code for each needed component and added a logo from a film, and css for each component

---
+++ src/Components/People.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+import "./People.css"
+
+export default function People() {
+
+  const [person, setPerson] = useState([])
+  const [selectedPerson, setSelectedPerson] = useState("");
+  const [toggleError, setToggleError] =useState(false)
+    
+  function handleTextChange(event) {
+    setSelectedPerson(event.target.value.slice(0,1).toUpperCase() + event.target.value.slice(1).toLowerCase());
+    console.log(selectedPerson)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+    
+    fetch(`https://resource-ghibli-api-pursuit.onrender.com/people/?name=${selectedPerson}`)
+      .then((res) => res.json())
+      .then((response) => setPerson(response));
+    setSelectedPerson('')
+    setToggleError(true)
+    }
+
+  return (
+    <div className="people">
+      <h1>Search for a Person</h1> 
+      <form onSubmit={handleSubmit}>
+        
+          <input
+          type="text"
+          value={selectedPerson}
+          placeholder="Search for a person"
+          onChange={handleTextChange}
+          id="name"
+        /> 
+        <button>Submit</button>
+        </form> 
+
+        <article className="handletoggle">
+        {
+            
+            !toggleError ? (null) :(
+        (person.length !==0) ?  (
+          <>
+        <p><span>Name:</span> {person[0].name}</p>
+        <p><span>Age:</span> {person[0].age}</p> 
+        <p><span>Gender:</span> {person[0].gender}</p>
+        </>) : ("Not found")
+        )}
+        </article>
+    </div>
+  )
+} 
+ 
