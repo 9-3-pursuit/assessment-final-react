6c22b776b06b22438f2b20451e51ffc7f587a5ae
pursuit-florence <florenceosei@pursuit.org>
2023-03-24 11:02:53 -0400

    final assessment

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,3 +1,74 @@
+// import { useState,useEffect } from "react"
+// // import "./People.css"
+
+
+// export default function People() {
+// const [selectedPerson, setSelectedPerson] = useState("");
+//   const [person, setPerson] = useState([]);
+//   const [toggle, setToggle] = useState(false)
+//   const [peopleList, setPeopleList] = useState([])
+//   useEffect(() => {
+//     fetch("https://resource-ghibli-api-pursuit.onrender.com/people")
+//       .then((res) => res.json())
+//       .then((response) => {
+//         setPeopleList(response)
+//       });
+// //         // }).catch((error) => {
+// //         //   console.log(error)
+// //         //   setError(error);
+//   }, []);
+  
+//   function handleTextChange(event) {
+//     setSelectedPerson(event.target.value.slice(0,1).toUpperCase() + event.target.value.slice(1).toLowerCase());
+//     console.log(selectedPerson)
+//   }
+
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     const baseURL = "https://resource-ghibli-api-pursuit.onrender.com";
+    
+//     fetch(`${baseURL}/people/?name=${selectedPerson}`)
+//        .then((res) => res.json())
+//        .then((response) => setPerson(response));
+//       setSelectedPerson("");
+//       setToggle(true)
+//   }
+    
+//   return (
+//     <div className="people">
+//       <h1>Search for a Person</h1> 
+//       <form onSubmit={handleSubmit}>
+        
+//           <input
+//           type="text"
+//           value={selectedPerson}
+//           placeholder="Search for a person"
+//           onChange={handleTextChange}
+//           id="name"
+//         /> 
+//         <button>Submit</button>
+//         </form>
+//         <article>
+//         {
+//           !toggle ? (null) :(person.length === 0) ? (<p className="not-found">Not found</p>) :
+//         (!person.length !== 0) ? (
+//           <>
+//         <p><span className="name">Name:</span> {person[0].name}</p>
+//         <p><span className="age">Age:</span> {person[0].age}</p> 
+//         <p><span className="gender">Gender:</span> {person[0].gender}</p>
+       
+//         <p><span className="eye-color">Eye Color:</span> {person[0].eye_color}</p>
+//         <p><span className="hair-color">Hair Color:</span> {person[0].hair_color}</p>
+//         </>) : (null)
+        
+//         }
+//         </article> 
+//     </div>
+//   )
+// }
+
+
 import React, { useState, useEffect } from "react";
 export default function People() {
   const [people, setPeople] = useState("");
@@ -63,4 +134,3 @@ export default function People() {
     </div>
   );
 };
-
