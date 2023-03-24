c73f7af894ef1fbf7568a4820c01d60f76fe80a0
HaroldF415 <harold.f415@gmail.com>
2023-03-24 00:21:17 -0400

    updated path

--- src/hooks/useGhibliData.js
+++ src/hooks/useGhibliData.js
@@ -3,7 +3,7 @@
 // my thinking was to use one hook for all of them, but I'm not sure if that's the best way to do it
 
 import { useState, useEffect } from "react";
-import { fetchGhibliData } from "../utils/fetchGhibliData";
+import { fetchGhibliData } from "../utils/fetchData";
 
 export const useGhibliData = (endpoint) => {
   const [ghibliData, setGhibliData] = useState([]);
