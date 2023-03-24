359be089784c965b201e8fb1ccf4e5d206cd2d8b
Karimah Reavis <Karimah586@gmail.com>
2023-03-24 09:55:39 -0400

    locations set up

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Movies from "./components/Movies";
 import Nav from "./components/Nav";
 import People from "./components/People";
+import Locations from "./components/Locations";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/movies" element={<Movies/>} />
           <Route path="/people" element={<People/>} />
+          <Route path="/locations" element={<Locations/>} />
           </Routes>
     </div>
     </Router>
---
+++ src/components/Locations.js
@@ -0,0 +1,9 @@
+export default function Locations (){
+    return (
+        <div className="locations">
+            <h2>List of Locations</h2>
+            <button type="submit">Show Locations</button>
+
+        </div>
+    )
+}
