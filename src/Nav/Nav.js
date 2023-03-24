a7ed3053688c5623b4e0beb56f009b7f83bcf49d
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-24 09:37:05 -0400

    Adds image logo

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
+import logo from "../Assets/pngwing.png";
 export default function Nav() {
   return (
     <nav>
-      <Link to="/"> Logo </Link>
+      <Link to="/">
+        {" "}
+        <img src={logo} alt="Toroto side profile"></img>
+      </Link>
       <Link to="/movies"> Movies </Link>
       <Link to="/people"> People </Link>
       <Link to="/locations"> Locations </Link>
