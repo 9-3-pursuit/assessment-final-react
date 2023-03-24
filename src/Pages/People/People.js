9561bc297f6afe01cf3f913362b8c454f8e1dea4
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-23 21:28:09 -0400

    Adds content to People component

--- src/Pages/People/People.js
+++ src/Pages/People/People.js
@@ -1,3 +1,41 @@
+import { useState } from "react";
+import useStudioGhibliAPI from "../../Helpers/useStudioGhibliAPI";
+
 export default function People() {
-  return;
+  const allPeople = useStudioGhibliAPI("people");
+  const [userInput, setUserInput] = useState("");
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handlePersonSearch = (e) => {
+    const personDetails = allPeople.find(({ name }) => {
+      return name.toLowerCase() === userInput.toLowerCase();
+    });
+
+    setSelectedOption(personDetails || "Not Found");
+  };
+
+  const handleInputChange = (e) => setUserInput(e.target.value);
+  return (
+    <main>
+      <section className="people">
+        <h2>Search for a Person</h2>
+        <input type="text" value={userInput} onChange={handleInputChange} />
+        <button onClick={handlePersonSearch}>Submit</button>
+        {selectedOption && <ResultsSection selectedOption={selectedOption} />}
+      </section>
+    </main>
+  );
 }
+
+const ResultsSection = ({ selectedOption }) => {
+  return selectedOption === "Not Found" ? (
+    <p>Not Found</p>
+  ) : (
+    <aside>
+      <h2>Name: {selectedOption.name}</h2>
+      <h6>Age: {selectedOption.age}</h6>
+      <h6>Gender: {selectedOption.gender}</h6>
+      <h6>Eye Color: {selectedOption.eye_color}</h6>
+    </aside>
+  );
+};
