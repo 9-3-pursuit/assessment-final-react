0b8d539ff0cc962aae5e7a23758dd3dbe1b9f79b
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-03-24 11:05:25 -0400

    Add pages to router.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import Home from "./Components/Home";
+import Locations from "./Components/Locations";
+import Movies from "./Components/Movies";
+import People from "./Components/People";
 function App() {
+  const [locations, setLocations] = useState([]);
+  fetch("https://resource-ghibli-api-pursuit.onrender.com/locations")
+    .then((results) => results.json())
+    .then((response) => setLocations(response));
   return (
     <div className="app">
-      <h1>Welcome to GhibliApp</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/locations" element={<Locations />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
