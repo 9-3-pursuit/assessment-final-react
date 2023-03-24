921113ae29ec23469971c2eabc9c23be43d382d0
HaroldF415 <harold.f415@gmail.com>
2023-03-23 22:40:29 -0400

    useGhibliData custom hook

---
+++ src/hooks/useGhibliData.js
@@ -0,0 +1,16 @@
+// People / Films / Locations / all use the `useEffect` hook to fetch data from the Ghibli API
+// and store it in the `data` state variable.
+// my thinking was to use one hook for all of them, but I'm not sure if that's the best way to do it
+
+import { useState, useEffect } from "react";
+import { fetchGhibliData } from "../utils/fetchGhibliData";
+
+export const useGhibliData = (endpoint) => {
+  const [ghibliData, setGhibliData] = useState([]);
+
+  useEffect(() => {
+    fetchGhibliData(endpoint).then((data) => setGhibliData(data));
+  }, [endpoint]);
+
+  return ghibliData;
+};
