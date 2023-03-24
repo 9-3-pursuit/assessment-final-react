8d120c83c799ebe7833627917991917fef55d767
Diandre Miller <millerdiandre@gmail.com>
2023-03-23 23:04:22 -0400

    completed assessment

--- src/Components/Locations.jsx
+++ src/Components/Locations.jsx
@@ -3,15 +3,120 @@ import { getLocations } from "../API/Fetch"
 
 const Locations = () => {
 
-    const [locations, setLocations] = useState([])
+    const [locations, setLocations] = useState([]);
+    const [showLocations, setShowLocations] = useState(false);
+    const [sortByName, setSortByName] = useState(false);
+    const [sortByTerrain, setSortByTerrain] = useState(false);
+    const [sortByClimate, setSortByClimate] = useState(false);
+
 
     useEffect(() => {
         getLocations()
             .then((data) => {
                 setLocations(data)
-                console.log('locations',data)
-        })
-    },[])
+                console.log('locations', data)
+            })
+    }, []);
+    
+    const handleToggleLocations = (e) => {
+        e.preventDefault()
+        setShowLocations(!showLocations);
+        setSortByName(!sortByName);
+        setSortByTerrain(!sortByTerrain);
+        setSortByClimate(!sortByClimate);
+    };
+
+    const handleSortByName = () => {
+        setLocations([...locations].sort((a, b) => {
+            const nameA = a.name.toLowerCase();
+            const nameB = b.name.toLowerCase();
+            if (nameA < nameB) {
+                return -1
+            }
+            if (nameA > nameB) {
+                return 1
+            }
+            return 0
+        }));
+    };
+
+    const handleSortByClimate = () => {
+        setLocations([...locations].sort((a, b) => {
+            const climateA = a.climate.toLowerCase();
+            const climateB = b.climate.toLowerCase();
+            if (climateA < climateB) {
+                return -1
+            }
+            if (climateA > climateB) {
+                return 1
+            }
+            return 0
+        }));
+    };
+
+    const handleSortByTerrain = () => {
+        setLocations([...locations].sort((a, b) => {
+            const terrainA = a.terrain.toLowerCase();
+            const terrainB = b.terrain.toLowerCase();
+            if (terrainA < terrainB) {
+                return -1
+            }
+            if (terrainA > terrainB) {
+                return 1
+            }
+            return 0
+        }));
+    };
+
+    console.log(22, locations)
+
+    return (
+        <form onSubmit={handleToggleLocations}>
+            <main className="locations">
+
+                <button>
+                    {showLocations ? 'Hide Locations' : 'Show Locations'}
+                </button>
+
+                {sortByName && (
+                    <button type='button' onClick={handleSortByName}>
+                        Sort by Name
+                    </button>
+                )}
+
+                 {sortByClimate && (
+                    <button type='button' onClick={handleSortByClimate}>
+                        Sort by Climate
+                    </button>
+                )}
+
+                 {sortByTerrain && (
+                    <button type='button' onClick={handleSortByTerrain}>
+                        Sort by Terrain
+                    </button>
+                )}
+
+                <ul>
+                    {showLocations &&
+                        locations.map((location) => {
+                           return  <li key={location.id}>
+                                <ul>
+                                    <li><span><span>{location.name}</span></span></li>
+                                    <li><span><span>{location.climate}</span></span></li>
+                                    <li><span><span>{location.terrain}</span></span></li>
+                                </ul>                           
+                        </li>
+                    })
+                    }
+            
+
+                </ul>
+
+            </main>
+
+        </form>
+    )
+
 
 }
 export default Locations
