2f16580460e0e050fb69f25461933ce54fb1f6ad
AlexTan21 <87435891+AlexTan21@users.noreply.github.com>
2023-03-24 09:53:41 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import "./App.css";
+import Nav from "./components/Nav.js";
+import Locations from "./components/Locations.js";
+import Movies from "./components/Movies.js";
+import Navbar from "./components/Navbar.js";
+import People from "./components/People.js";
+
+
 function App() {
   return (
     <div className="app">
       <h1>Welcome to GhibliApp</h1>
+      <Navbar />
+      <Switch>
+        <Route path="/nav" component={Nav} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/people" component={People} />
+        <Route path='/locations' component={Locations} />
+      </Switch>
     </div>
   );
 }
